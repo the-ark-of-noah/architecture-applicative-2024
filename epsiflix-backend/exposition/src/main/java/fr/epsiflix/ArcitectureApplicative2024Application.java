4d98ffc952a717204c950c3f8b21f2c9c356328f
Noodles-boop <72260596+DevDeNiro@users.noreply.github.com>
2024-05-04 20:31:03 +0200

    init project

---
+++ epsiflix-backend/exposition/src/main/java/fr/epsiflix/ArcitectureApplicative2024Application.java
@@ -0,0 +1,13 @@
+package fr.epsiflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcitectureApplicative2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcitectureApplicative2024Application.class, args);
+	}
+
+}
