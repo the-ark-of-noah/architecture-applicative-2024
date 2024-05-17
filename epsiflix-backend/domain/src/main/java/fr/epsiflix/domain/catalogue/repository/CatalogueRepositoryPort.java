4049c7149dcbdea2f87abf4fd1e7a46d0ea79dc6
DevDeNiro <oldoni.jeremy@outlook.fr>
2024-05-17 14:47:41 +0200

    Update CatalogueRepositoryPort.java

--- epsiflix-backend/domain/src/main/java/fr/epsiflix/domain/catalogue/repository/CatalogueRepositoryPort.java
+++ epsiflix-backend/domain/src/main/java/fr/epsiflix/domain/catalogue/repository/CatalogueRepositoryPort.java
@@ -1,12 +1,15 @@
 package fr.epsiflix.domain.catalogue.repository;
 
-import fr.epsiflix.domain.catalogue.model.Product;
 import fr.epsiflix.domain.shared.UniqueId;
 
-public interface CatalogueRepository {
+public interface CatalogueRepositoryPort {
     void ajouterVideo(Video video);
+
     void supprimerVideo(UniqueId id);
+
     void modifierVideo(UniqueId id, Video video);
+
     List<Video> listerVideos();
+
     List<Video> listerVideosParCategories(String categorie);
 }
